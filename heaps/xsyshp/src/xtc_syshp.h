9f0c1e25bd514695d2c433bd614ec7267060a8b5
thek <soft.thek@gmail.com>
2023-05-18 03:11:58 +0200

    Modified header for C++ compatibility

--- heaps/xsyshp/src/xtc_syshp.h
+++ heaps/xsyshp/src/xtc_syshp.h
@@ -1,9 +1,17 @@
 #ifndef _XTC_SYSHP_H_
 #define _XTC_SYSHP_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "xtc_gnrhp.h"
 
 // System heap in the XTC form
 xtc_heap_t* sys_heap();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // _XTC_SYSHP_H_
