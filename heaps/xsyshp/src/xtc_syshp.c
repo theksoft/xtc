509351d5be411fd4c1494a2a61855d480f224e17
thek <soft.thek@gmail.com>
2023-07-25 00:00:29 +0200

    XTC#73 Implement system heap allocator

--- heaps/xsyshp/src/xtc_syshp.c
+++ heaps/xsyshp/src/xtc_syshp.c
@@ -1,53 +1,197 @@
-#include "xtc_syshp.h"
-#include <stdint.h>
-
 /*
   System heap in the XTC form.
+  For tracking memory issue, prefer the use of a standard memory tracker tool.
 */
+
+#include "xtc_syshp.h"
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+
+//==================================================================================================
+// Local inline functions
+//==================================================================================================
 
 static xtc_heaps_id_t get_id() {
   static unsigned int ref = 0x0FEEDBABE;
   return (xtc_heaps_id_t)(uintptr_t)&ref;
 }
 
-static inline xtc_heap_t* check(xtc_heap_t *this) {
-  return this && this->id == get_id() ? this : NULL;
+static inline xss_heap_t* check(xss_heap_t *this) {
+  return this && this->interface.id == get_id() ? this : NULL;
 }
 
-#ifndef __DEBUG
+static xss_node_t* get_node(xss_heap_t *this, void *ptr);
 
-static void* sys_alloc(xtc_heap_t *this, size_t size) {
-  void *rtn = NULL;
-  xtc_heap_t *heap = check(this);
-  if (heap) {
-    rtn = malloc(size);
+#ifdef __DEBUG
+static void dump(xss_heap_t *heap);
+#endif
+
+static void dummy() {}
+
+//==================================================================================================
+// Interface functions
+//==================================================================================================
+
+xtc_heap_t* xss_init(xss_heap_t *this, xtc_protect_t *protect) {
+  xtc_heap_t *rtn = NULL;
+  if (this) {
+    if (!protect || (protect->lock && protect->unlock)) {
+      this->interface.id = get_id();
+#ifndef __DEBUG
+      this->interface.alloc = (xtc_alloc_t)xss_alloc;
+#else
+      this->head = NULL;
+      this->interface.dump = (xtc_dump_t)xss_dump;
+      this->interface.alloc_dbg = (xtc_alloc_t)xss_alloc_dbg;
+#endif
+      this->interface.free = (xtc_free_t)xss_free;
+      this->interface.count = (xtc_count_t)xss_count;
+      this->interface.end = (xtc_end_t)xss_end;
+      this->count = 0;
+      this->total_size = 0;
+      this->protect.lock = (protect) ? protect->lock : dummy;
+      this->protect.unlock = (protect) ? protect->unlock : dummy;
+      rtn = &this->interface;
+    }
   }
   return rtn;
 }
 
-#endif
-
-static void sys_free(xtc_heap_t *this, void *ptr) {
-  xtc_heap_t *heap = check(this);
+void* xss_end(xss_heap_t *this, size_t *count) {
+  xss_heap_t *heap = check(this);
   if (heap) {
-    free(ptr);
+    xtc_protect_t protect = heap->protect;
+    protect.lock();
+    size_t cnt = heap->count;
+    if (count) {
+      *count = cnt;
+    }
+#ifdef __DEBUG
+    if (cnt) {
+      dump(heap);
+    }
+#endif
+    memset(heap, 0, sizeof(xss_heap_t));
+    protect.unlock();
+    if (cnt) {
+      fprintf(stderr, "WARNING: %ld memory leak(s) detected when ending heap!\n", cnt);
+    }
+  }
+  return NULL;
+}
+
+#ifndef __DEBUG
+void* xss_alloc(xss_heap_t *this, size_t size)
+#else
+void* xss_alloc_dbg(xss_heap_t *this, size_t size, const char *fn, int line)
+#endif
+{
+  void *rtn = NULL;
+  xss_heap_t *heap = check(this);
+  if (heap && size) {
+    heap->protect.lock();
+    xss_node_t *node = malloc(size + sizeof(xss_node_t));
+    if (node) {
+      memset(node, 0, sizeof(xss_node_t));
+      node->heap = (uintptr_t)heap;
+      node->size = size;
+      heap->total_size += size;
+      heap->count++;
+#ifdef __DEBUG
+      node->fn = fn;
+      node->line = line;
+      node->previous = NULL;
+      node->next = heap->head;
+      if (heap->head->previous) {
+        heap->head->previous = node;
+      }
+      heap->head = node;
+#endif
+      rtn = (void*)(node + 1);
+    }
+    heap->protect.unlock();
+  }
+  return rtn;
+}
+
+void xss_free(xss_heap_t *this, void *ptr) {
+  xss_heap_t *heap = check(this);
+  if (heap && ptr) {
+    heap->protect.lock();
+    xss_node_t *node = get_node(heap, ptr);
+    if (node) {
+      heap->count--;
+      heap->total_size -= node->size;
+#ifdef __DEBUG
+      if (heap->head == node) {
+        heap->head = node->next;
+      }
+      if (node->previous) {
+        node->previous->next = node->next;
+      }
+      if (node->next) {
+        node->next->previous = node->previous;
+      }
+#endif
+      memset(node, 0, sizeof(xss_node_t));
+      free(node);
+    }
+    heap->protect.unlock();
   }
 }
 
-xtc_heap_t* sys_heap() {
-  static int initialized = 0;
-  static xtc_heap_t heap = {
-    .id = 0,
-  #ifndef __DEBUG
-    .alloc = sys_alloc,
-  #else
-    .alloc_dbg = NULL,
-  #endif
-    .free = sys_free
-  };
-  if (!initialized) {
-    heap.id = get_id();
-    initialized = 1;
+size_t xss_count(xss_heap_t *this) {
+  size_t rtn = 0;
+  xss_heap_t *heap = check(this);
+  if (heap) {
+    heap->protect.lock();
+    rtn = heap->count;
+    heap->protect.unlock();
   }
-  return &heap;
+  return rtn;
 }
+
+size_t xss_total_size(xss_heap_t *this) {
+  size_t rtn = 0;
+  xss_heap_t *heap = check(this);
+  if (heap) {
+    heap->protect.lock();
+    rtn = heap->total_size;
+    heap->protect.unlock();
+  }
+  return rtn;
+}
+
+//==================================================================================================
+// Local functions implementation
+//==================================================================================================
+
+static xss_node_t* get_node(xss_heap_t *heap, void *ptr) {
+  xss_node_t *rtn = NULL;
+  xss_node_t *tmp = (xss_node_t*)ptr - 1;
+  if (tmp->heap == (uintptr_t)heap) {
+    rtn = tmp;
+  }
+  return rtn;
+}
+
+#ifdef __DEBUG
+
+/**
+ * @brief Dump the heap allocated block information.
+ * 
+ * @param heap System heap
+ */
+
+static void dump(xss_heap_t *heap) {
+  xss_node_t *node;
+  printf("HEAP SYS, Blks. %p[%ld] - %ld bytes\n",
+    (void*)heap->head, heap->count, heap->total_size);
+  for (node = heap->head; NULL != node; node = node->next) {
+    printf("\t%p[%ld] @ %s (%d)\n",
+      (void*)(node + 1), node->size, node->fn, node->line);
+  }
+}
+
+#endif
--- heaps/xsyshp/src/xtc_syshp.h
+++ heaps/xsyshp/src/xtc_syshp.h
@@ -1,14 +1,57 @@
 #ifndef _XTC_SYSHP_H_
 #define _XTC_SYSHP_H_
 
+#include "xtc_gnrhp.h"
+#include <stdint.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#include "xtc_gnrhp.h"
+typedef struct xss_node_s {
+  uintptr_t heap;
+  size_t size;
+#ifdef __DEBUG
+  struct xss_node_s *next, *previous;
+  const char *fn;
+  int line;
+#endif
+} xss_node_t;
 
-// System heap in the XTC form
-xtc_heap_t* sys_heap();
+typedef struct {
+  xtc_heap_t interface;
+  xtc_protect_t protect;
+  size_t count;
+  size_t total_size;
+#ifdef __DEBUG
+  xss_node_t *head;
+#endif
+} xss_heap_t;
+
+xtc_heap_t* xss_init(xss_heap_t *heap, xtc_protect_t *protect);
+
+void* xss_end(xss_heap_t *heap, size_t *count);
+
+#ifndef __DEBUG
+
+void* xss_alloc(xss_heap_t *heap, size_t size);
+
+#else // __DEBUG
+
+void* xss_alloc_dbg(xss_heap_t *heap, size_t size, const char *fn, int line);
+#define xss_alloc(heap, size)     xss_alloc_dbg(heap, size, __FILE__, __LINE__)
+
+#endif  // __DEBUG
+
+void xss_free(xss_heap_t *heap, void *ptr);
+
+size_t xss_count(xss_heap_t *heap);
+
+size_t xss_total_size(xss_heap_t *heap);
+
+#ifdef __DEBUG
+void xss_dump(xss_heap_t *heap);
+#endif  // __DEBUG
 
 #ifdef __cplusplus
 }
